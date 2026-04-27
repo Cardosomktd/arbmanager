6d8cd791f286cfeabad03833dac84fe93901cd41
Gustavo <cardosomktd@gmail.com>
2026-04-27 20:05:32 -0300

    chore: reforca configuracoes de producao

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    // Nunca gerar sourcemaps no bundle de produção
+    sourcemap: false,
+  },
+
+  esbuild: {
+    // Remove automaticamente console.* e debugger do bundle de produção
+    drop: ['console', 'debugger'],
+  },
 })
